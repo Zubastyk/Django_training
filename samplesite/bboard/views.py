3fd90e1a861969bbdef17f1b3e64a0c9bf2b9599
zubastyk <zubbastyk@gmail.com>
2023-05-14 20:57:49 +0300

    Добавлен class BbByRubricView, который выводит страницу с объявлениями из выбранной посетителем рубрики Пофичены одреса шаблонов в контроллерах, убраны в папку bboard

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,8 +1,10 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy, reverse
+from django.views.generic.list import ListView
 
 from .models import Bb, Rubric
 from .forms import BbForm
@@ -16,7 +18,7 @@ def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
     context = {'bbs' : bbs, 'rubrics' : rubrics}
-    return render(request, 'index.html', context)
+    return render(request, 'bboard/index.html', context)
 
 
 def by_rubric(request, rubric_id):
@@ -24,11 +26,11 @@ def by_rubric(request, rubric_id):
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs' : bbs, 'rubrics' : rubrics, 'current_rubric' : current_rubric}
-    return render(request, 'by_rubric.html', context)
+    return render(request, 'bboard/by_rubric.html', context)
 
 
 class BbCreateView(CreateView):
-    template_name = 'create.html'
+    template_name = 'bboard/create.html'
     form_class = BbForm
     success_url = reverse_lazy('index')
     
@@ -45,4 +47,21 @@ class BbDetailView(DetailView):
     def get_context_data(self, **kwargs): 
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
+        return context
+    
+    
+# выводит страницу с объявлениями из выбранной посетителем рубрики
+class BbByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+    
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(
+                                    pk=self.kwargs['rubric_id']
+        )
         return context
