4ecf0d846ef4429432b24224a884795940aabc48
zubastyk <zubbastyk@gmail.com>
2023-05-20 22:15:19 +0300

    не доделан BbRubricView

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -58,17 +58,24 @@ class BbDetailView(DetailView):
 class BbByRubricView(ListView):
     template_name = 'bboard/by_rubric.html'
     context_object_name = 'bbs'
+   
     
+    
+        
     def get_queryset(self):
         return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
-    
+    def get_ordering(self):
+            ordering =  Bb.objects.filter(rubric=self.kwargs['rubric_id'])('ordering', '-published')
+            # validate ordering here
+            print("сортировка")
+            return ordering
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         context['current_rubric'] = Rubric.objects.get(
                                     pk=self.kwargs['rubric_id'])
         return context
-
+    
 #Добавляет на виртуальную доску новое объявление и сохраняет его 
 class BbAddView(FormView):
     template_name = 'bboard/create.html'
